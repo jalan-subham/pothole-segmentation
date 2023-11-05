9325be502ca3ecfbd30410790c5057b78211fd17
jalan-subham <114836590+jalan-subham@users.noreply.github.com>
2023-11-05 19:12:02 +0530

    fixed mode.py

--- pyimagesearch/model.py
+++ pyimagesearch/model.py
@@ -91,19 +91,19 @@ class UNet(Module):
 		self.outSize = outSize
 
 
-def forward(self, x):
-		# grab the features from the encoder
-		encFeatures = self.encoder(x)
-		# pass the encoder features through decoder making sure that
-		# their dimensions are suited for concatenation
-		decFeatures = self.decoder(encFeatures[::-1][0],
-			encFeatures[::-1][1:])
-		# pass the decoder features through the regression head to
-		# obtain the segmentation mask
-		map = self.head(decFeatures)
-		# check to see if we are retaining the original output
-		# dimensions and if so, then resize the output to match them
-		if self.retainDim:
-			map = F.interpolate(map, self.outSize)
-		# return the segmentation map
-		return map
+	def forward(self, x):
+			# grab the features from the encoder
+			encFeatures = self.encoder(x)
+			# pass the encoder features through decoder making sure that
+			# their dimensions are suited for concatenation
+			decFeatures = self.decoder(encFeatures[::-1][0],
+				encFeatures[::-1][1:])
+			# pass the decoder features through the regression head to
+			# obtain the segmentation mask
+			map = self.head(decFeatures)
+			# check to see if we are retaining the original output
+			# dimensions and if so, then resize the output to match them
+			if self.retainDim:
+				map = F.interpolate(map, self.outSize)
+			# return the segmentation map
+			return map
